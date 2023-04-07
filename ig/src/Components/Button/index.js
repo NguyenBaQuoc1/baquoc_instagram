633b1048f64e92b3f3793df0a5ec9fb49ea948a3
quoc <nguyenbaquoc.hanoi@gmail.com>
2023-04-08 04:28:11 +0700

    a
    a

--- ig/src/Components/Button/index.js
+++ ig/src/Components/Button/index.js
@@ -3,6 +3,19 @@ import classNames from 'classnames/bind'
 
 const cx = classNames.bind(styles)
 
-function Button() {
-    
+function Button({children , signin}) {
+    const Comp = 'button'
+
+
+    const clasname = cx('wrapper' , {
+        signin
+    })
+
+    return (
+        <Comp className={clasname} >
+            <span className={cx('title')}>{children}</span>
+        </Comp>
+    )
 }
+
+export default Button
--- ig/src/Components/Layout/Header/index.js
+++ ig/src/Components/Layout/Header/index.js
@@ -1,6 +1,11 @@
 import Logo from '../../Icon/logo'
 import styles from './Header.module.scss'
 import classNames from 'classnames/bind'
+import { faEllipsisVertical, faMicrophone, faSearch, faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Button from '../../Button'
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
 
 const cx = classNames.bind(styles)
 
@@ -13,11 +18,30 @@ function Header() {
                 </div>
 
                 <div className={cx('search')}>
-                    <input />
+                    <div className={cx('icon-search')}><FontAwesomeIcon icon={faSearch}/></div>
+                    <input placeholder='Search' className={cx('input-search')}/>
+                    <div className={cx('microphone')}><FontAwesomeIcon icon={faMicrophone} /></div>
                 </div>
 
                 <div className={cx('action')}>
-                    <Fon
+
+                    <Tippy
+                        interactive
+                        render={attrs => (
+                            <div className="box" tabIndex="-1" {...attrs}>
+                                My tippy box
+                            </div>
+                            )}
+                    >
+                        <Button><FontAwesomeIcon icon={faEllipsisVertical}/></Button>
+                    </Tippy>
+
+                    <Button signin>
+                        <span className={cx('icon')}>
+                            <FontAwesomeIcon icon={faUser}/>
+                        </span>
+                        Sign In
+                    </Button>
                 </div>
             </div>
         </div> 
